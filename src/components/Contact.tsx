a672fcd578184266cef0b688c081b4837e312cf6
purimsq <dylengitaka@gmail.com>
2026-01-08 01:11:53 +0300

    feat: Success final update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -76,7 +76,7 @@ export default function Contact() {
                                 {status === 'sending' ? (
                                     'Sending...'
                                 ) : status === 'success' ? (
-                                    <>Sent! I will get back to you as soon <Check size={20} /></>
+                                    <>Sent! I will get back to you soon <Check size={20} /></>
                                 ) : (
                                     <>Send Request <Send size={20} /></>
                                 )}
